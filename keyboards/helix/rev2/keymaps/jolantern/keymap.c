542e81a30a42a226404445a0d97830b3af786cff
j-o-lantern0422 <j-o-lantern0422@gmail.com>
2018-08-10 10:28:44 +0900

    RAISEも使うことにした

--- keyboards/helix/rev2/keymaps/jolantern/keymap.c
+++ keyboards/helix/rev2/keymaps/jolantern/keymap.c
@@ -34,7 +34,7 @@ enum layer_number {
     _COLEMAK,
     _DVORAK,
     _LOWER = 3,
-    _RAISE,
+    _RAISE = 4,
     _ADJUST = 6
 };
 
@@ -83,9 +83,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,   KC_E,    KC_R,   KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    JP_AT,   \
       KC_LCTL, KC_A,    KC_S,   KC_D,    KC_F,   KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,   JP_SCLN, JP_COLN, \
       KC_LSFT, KC_Z,    KC_X,   KC_C,    KC_V,   KC_B,    JP_LBRC, JP_RBRC, KC_N,    KC_M,    JP_COMM, JP_DOT, JP_SLSH, JP_UNDS, \
-      ADJUST,  KC_LGUI,  KC_LALT, LOWER,   EISU, KC_SPC, KC_ENT,  KC_ENT,   KC_SPC,    KANA,   KC_BSPC,   KC_BSPC,  JP_CIRC, JP_YEN   \
+      ADJUST,  KC_LGUI,  KC_LALT, LOWER,   EISU, KC_SPC, KC_ENT,  KC_ENT,   KC_SPC,    KANA,   KC_BSPC,   KC_BSPC,  JP_CIRC, RAISE   \
       ),
-
 
   /* Lower
    * ,-----------------------------------------.             ,-----------------------------------------.
@@ -101,12 +100,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_LOWER] = KEYMAP( \
-      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
+      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, JP_YEN, \
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_LPRN, KC_RPRN, KC_F12,  _______, _______, KC_HOME, KC_UP,  _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_LEFT, KC_DOWN, KC_RGHT \
       ),
+
+  /* Raise
+   * ,-----------------------------------------.             ,-----------------------------------------.
+   * |   `  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  | Bksp |
+   * |------+------+------+------+------+------|             |------+------+------+------+------+------|
+   * |   `  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  | Del  |
+   * |------+------+------+------+------+------|             |------+------+------+------+------+------|
+   * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |   -  |   =  |   [  |   ]  |  \   |
+   * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
+   * |      |  F7  |  F8  |  F9  |  F10 |  F11 |      |      |  F12 |      |      |PageDn|PageUp|      |
+   * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
+   * |      |      |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
+   * `-------------------------------------------------------------------------------------------------'
+   */
+  [_RAISE] = KEYMAP( \
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_0,    KC_BSPC, \
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_7,    KC_8,    KC_9,    KC_9,    KC_0,    KC_DEL, \
+      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_4,    KC_5,    KC_6,  KC_LBRC, KC_RBRC, KC_BSLS, \
+      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_1,    KC_2,    KC_3,   KC_PGDN, KC_PGUP, _______, \
+      _______, _______, _______, _______, _______, _______, _______, KC_ENT, KC_0,    KC_0,    JP_DOT, KC_VOLD, KC_VOLU, KC_MPLY \
+      ),
+
+
 
   /* Adjust (Lower + Raise)
    * ,-----------------------------------------.             ,-----------------------------------------.
