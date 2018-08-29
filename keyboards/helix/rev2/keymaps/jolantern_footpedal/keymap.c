3ea235c8b50ef1166eb2a936304177a31df194e7
j-o-lantern0422 <j-o-lantern0422@gmail.com>
2018-08-29 14:46:05 +0900

    キーマップもキーが一つ増えた想定に対応する

--- keyboards/helix/rev2/keymaps/jolantern_footpedal/keymap.c
+++ keyboards/helix/rev2/keymaps/jolantern_footpedal/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = KEYMAP( \
-      KC_ESC,  KC_1,    KC_2,   KC_3,    KC_4,   KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    JP_MINS, \
+      KC_ESC,  KC_1,    KC_2,   KC_3,    KC_4,   KC_5,   RAISE,             KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    JP_MINS, \
       KC_TAB,  KC_Q,    KC_W,   KC_E,    KC_R,   KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    JP_AT,   \
       KC_LCTL, KC_A,    KC_S,   KC_D,    KC_F,   KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,   JP_SCLN, JP_COLN, \
       KC_LSFT, KC_Z,    KC_X,   KC_C,    KC_V,   KC_B,    JP_LBRC, JP_RBRC, KC_N,    KC_M,    JP_COMM, JP_DOT, JP_SLSH, JP_UNDS, \
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_LOWER] = KEYMAP( \
-      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, JP_YEN, \
+      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  _______,            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, JP_YEN, \
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_LPRN, KC_RPRN, KC_F12,  _______, _______, KC_HOME, KC_UP,  _______, \
@@ -121,7 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_RAISE] = KEYMAP( \
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_0,    KC_BSPC, \
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     _______,             KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_0,    KC_BSPC, \
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_7,    KC_8,    KC_9,    KC_9,    KC_0,    KC_DEL, \
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_4,    KC_5,    KC_6,  KC_LBRC, KC_RBRC, KC_BSLS, \
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_1,    KC_2,    KC_3,   KC_PGDN, KC_PGUP, _______, \
@@ -144,7 +144,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_ADJUST] =  KEYMAP( \
-      _______, _______,   KC_UP,   _______,   _______,   _______,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
+      _______, _______,   KC_UP,   _______,   _______,   _______, _______,            KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
       _______, KC_LEFT, KC_UP, KC_RGHT, _______, _______,                   _______, _______, _______, _______, _______, KC_DEL, \
       _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,  AG_NORM,                   AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, \
