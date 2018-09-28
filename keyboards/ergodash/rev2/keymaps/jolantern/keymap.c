8844f5cc984e2565a056c2be07d6ffaa560f7498
j-o-lantern0422 <j-o-lantern0422@gmail.com>
2018-09-28 09:35:10 +0900

    逆かなー

--- keyboards/ergodash/rev2/keymaps/jolantern/keymap.c
+++ keyboards/ergodash/rev2/keymaps/jolantern/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL ,                        KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    EISU ,                          KANA,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_LGUI, KC_LALT, RAISE,   LOWER,            KC_ENT,  KC_SPC ,_______,        _______,KC_SPC , KC_ENT,           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+    KC_LGUI, KC_LALT, RAISE,   LOWER,            KC_SPC,  KC_ENT ,_______,        _______,KC_ENT , KC_SPC,           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
   ),
 
   /* Lower
