a9a86bfb361dbf268adac5755a3eb4959ec86e3d
j-o-lantern0422 <j.o.lantern0422@gmail.com>
2020-03-16 16:36:20 +0900

    layer移動できるようになった

--- keyboards/ergodash/rev2/keymaps/jolantern/keymap.c
+++ keyboards/ergodash/rev2/keymaps/jolantern/keymap.c
@@ -14,6 +14,7 @@ enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
+  TG_LOW,
   EN,
   JA,
   EISU,
@@ -46,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL ,                        KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,   JP_SCLN, JP_COLN, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    EISU ,                          KANA,    KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH, JP_YEN, \
-    KC_LGUI, KC_LALT, RAISE,   LOWER,            KC_SPC,  KC_LSFT ,_______,        _______,KC_LGUI , KC_ENT,           KC_BSPC, KC_DEL, JP_UNDS,   ADJUST  \
+    KC_LGUI, KC_LALT, RAISE,   LOWER,            KC_SPC,  KC_LSFT ,_______,        _______,KC_RGUI , KC_ENT,           KC_BSPC, KC_DEL, JP_UNDS,   ADJUST  \
   ),
 
   /* Lower
@@ -106,8 +107,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_ADJUST] = LAYOUT(
     _______, _______, _______, _______, _______, _______,_______,                       _______, _______,  KC_EQL, _______, _______, _______, _______, \
-    _______, _______  ,KC_UP, _______, _______, RGB_HUI,_LOWER,                       _LOWER, RGB_SAD,   KC_7, KC_8, KC_9, _______, _______, \
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______ , BL_DEC ,_QWERTY,                       _QWERTY, _______, KC_4, KC_5, KC_6, _______, _______, \
+    _______, _______  ,KC_UP, _______, _______, RGB_HUI, TO(_LOWER),                        TO(_LOWER), RGB_SAD,   KC_7, KC_8, KC_9, _______, _______, \
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______ , BL_DEC ,TO(_QWERTY),                       TO(_QWERTY), _______, KC_4, KC_5, KC_6, _______, _______, \
     _______, _______, _______, _______, _______, _______,_______,                       _______, _______,  KC_1, KC_2, KC_3, _______, _______, \
     _______, _______, _______, _______,          _______,_______,_______,       _______,_______, _______,        KC_0, JP_DOT, _______, _______  \
   )
